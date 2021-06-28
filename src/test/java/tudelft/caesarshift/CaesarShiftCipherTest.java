a20eeff792513530b0dc7014d325a5d10ad084ab
Cepa <cepa.repositorios@gmail.com>
2021-06-28 13:48:35 -0500

    I tried to solve but i cannot do it, i am going to try until i can finish

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,29 @@
 package tudelft.caesarshift;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CaesarShiftCipherTest {
+ @Test
+ public void cSShort(String message) {
+  CaesarShiftCipher cS = new CaesarShiftCipher();
+   cS.CaesarShiftCipher("abc");
+   Assertions.asserEQuals("def", message);
+ }
+ //I tried to solve but i cannot do it, i am going to try until i can finish
+/*
+    @ParameterizedTest(name = "message={0}, shift={1},")
+    @CsvSource({
+            "abc, 1"
+    })
+    public void cSShort(String message, int shift, String expectResult){
+        CaesarShiftCipher cS = new CaesarShiftCipher();
+          cS.CaesarShiftCipher(message, shift).toString();
+        Assertions.assertEquals(expectResult, message);
+
+    }
+    */
 
 }
